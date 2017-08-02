9272bade9ff02b6b4104071d479bd37618f17744
2017Jose <gordoalfaro@gmail.com>
2017-08-02 04:20:51 -0600

    menu

---
+++ menu.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta charset="UTF-8">
+<!-- Latest compiled and minified CSS -->
+        <link rel="icon" type="image/png" href="https://upload.wikimedia.org/wikipedia/en/1/1f/Universidad_Nacional_Costa_Rica.png" />
+        <LINK REL="stylesheet" TYPE="text/css" HREF="css/diseno.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+        
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="//netdna.bootstrapcdn.com/bootstrap/2.3.2/js/bootstrap.min.js"></script>
+
+
+	<title>Reclutamiento de Profesores, Campus Sarapiqui</title>
+
+</head>
+<body>
+
+<nav class="navbar navbar-default navbar-fixed-top" id="cajaCabecera">
+  <div class="container" >
+   	<p>Reclutamiento de profesores UNA</p>
+  </div>
+</nav>
+
+</br>
+<hr/>
+  <nav class="navbar navbar-default" role="navigation" id="menu" >
+    <!-- El logotipo y el icono que despliega el menú se agrupan
+         para mostrarlos mejor en los dispositivos móviles -->
+  <div class="navbar"> 
+    <div class="navbar-header" >
+      <button type="button" class="navbar-toggle" data-toggle="collapse"
+              data-target=".navbar-ex1-collapse">
+        <span class="sr-only">Desplegar navegación</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+  
+    <!-- Agrupar los enlaces de navegación, los formularios y cualquier
+         otro elemento que se pueda ocultar al minimizar la barra -->
+    <div class="collapse navbar-collapse navbar-ex1-collapse" id="menuDentro">
+      <ul class="nav navbar-nav">
+      <li><a href="#" id="inicio">Inicio</a></li>
+       <li><a href="#" id="contactenos">Contactenos</a></li>
+       <li><a href="#" id="mision">Mision</a></li>
+       <li><a href="#" id="vision">Vision</a></li>
+       <li><a href="#" id="ubicacion">Ubicacion</a></li>
+      </ul>
+       
+      <ul class="nav navbar-nav navbar-right">
+          <li ><a href="#" id="iniciar">Iniciar</a></li>
+          <li><a href="#" id="registrarme">Registrarme</a></li> 
+      </ul>
+    </div>
+    </div>
+  </nav>
+</body>
+</html>
