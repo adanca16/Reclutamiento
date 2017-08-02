bf1c9ad6b6818cab9f912a395f6e6e344f65dc90
Adan Carranza Alfaro <adanca16@gmail.com>
2017-08-01 23:40:22 -0600

    Avence concluido del index

--- index.php
+++ index.php
@@ -15,7 +15,8 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-        <link rel="stylesheet" href="css/estilos.css">
+        <link rel="stylesheet" href="css/diseno.css">
+
 
 
   <!-- MATERIALIZE  Compiled and minified CSS -->
@@ -29,24 +30,63 @@
     <body>
 
 <div class="row" id="menuPrincipal" style  = "background-color:#cc071e; ">
-      <div class=" col-md-2  col-xs-5" align="center">
-        <img src="pictures/logounablancopeq.png" alt="" width="50%" height="80%">
+      <div class=" col-md-3  col-xs-4" align="center">
+        <img src="pictures/logounablancopeq.png" class="img-rounded" alt="Cinque Terre" width="150" height="150">
       </div>
-      <div class="col-sm-8 col-xs-6" align="center" >
-     	<h1  style="color:#ffffff; font-size: 4vw;"  >Bolsa de Empleo UNA</h1>
-        
+
+      <div class="col-md-7 col-xs-6" align="center" >
+     	<h1  style="color:#ffffff; font-size: 4vw;"  >Bolsa de Empleo UNA</h1>  
       </div>
-      <div class="col-sm-2 col-xs-1" >
-        <img src="pictures/logounablancopeq.png" alt="" width="50%" height="80%">
+
+      <div class="col-md-2 col-xs-2"  >
+        <img src="pictures/logounacolegio.png"  class="img-rounded" alt="Cinque Terre" width="150" height="150">
       </div>
 </div>
 
-<div class="row" ">
-PARTE DE PABLOOOO
-</div>
-     
+  <nav class="navbar navbar-default" role="navigation" id="menu" >
+    <!-- El logotipo y el icono que despliega el menú se agrupan
+         para mostrarlos mejor en los dispositivos móviles -->
+  <div class="navbar"> 
+    <div class="navbar-header" >
+      <button type="button" class="navbar-toggle" data-toggle="collapse"
+              data-target=".navbar-ex1-collapse">
+        <span class="sr-only">Desplegar navegación</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+  
+    <!-- Agrupar los enlaces de navegación, los formularios y cualquier
+         otro elemento que se pueda ocultar al minimizar la barra -->
+    <div class="collapse navbar-collapse navbar-ex1-collapse" id="menuDentro">
+      <ul class="nav navbar-nav">
+      <li><a href="#" id="inicio">Inicio</a></li>
+       <li><a href="#" id="contactenos">Contactenos</a></li>
+       <li><a href="#" id="mision">Mision</a></li>
+       <li><a href="#" id="vision">Vision</a></li>
+       <li><a href="#" id="ubicacion">Ubicacion</a></li>
+      </ul>
+       
+      <ul class="nav navbar-nav navbar-right">
+          <li ><a href="#" id="iniciar">Iniciar</a></li>
+          <li><a href="#" id="registrarme">Registrarme</a></li> 
+      </ul>
+    </div>
+    </div>
+  </nav>
 
-    <header id="myCarousel" class="carousel slide">
+<!-- Capa del carrusel con el menu de anuncioss-->
+ 
+
+
+
+
+  <div class="container-fluid">
+  <div class="row">
+  	<div class="col-sm-9 col-xs-12">      <!-- Primer bloque de 4 columnas -->
+
+    <div id="myCarousel" class="carousel slide">
         <!-- Indicators -->
         <ol class="carousel-indicators">
             <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
@@ -59,22 +99,22 @@ PARTE DE PABLOOOO
         <div class="carousel-inner">
             <div class="item active">
                 <!-- Set the first background image using inline CSS below. -->
-                <div class="fill" style="background-image:url('http://www.una.ac.cr/images/resized/images/sampledata/slideshow/022%20Banner%20PAG%20WEB%20INST-%20cita%20calendario_960_278.png ');"></div>
+                <div class="fill" style="background-image:url('https://scontent.fsjo1-1.fna.fbcdn.net/v/t1.0-9/17457643_628480387337815_5458830816509658061_n.jpg?oh=e64b0a3caa04453993736a524f03def8&oe=59F9EE60');"></div>
             </div>
             <div class="item">
                 <!-- Set the second background image using inline CSS below. -->
-                <div class="fill" style="background-image:url('http://www.una.ac.cr/images/resized/images/sampledata/slideshow/023%20Banner%20PAG%20WEB%20INST-%20cita%20calendario_960_278.png ');"></div>
+                <div class="fill" style="background-image:url('https://scontent.fsjo1-1.fna.fbcdn.net/v/t1.0-9/18157605_647452385440615_7957168046306641460_n.jpg?oh=5ba08d16427e9a5fbea8aaa8329d535a&oe=59FB6BD1');"></div>
              
             </div>
             <div class="item">
                 <!-- Set the third background image using inline CSS below. -->
-                <div class="fill" style="background-image:url('http://www.una.ac.cr/images/resized/images/sampledata/slideshow/021%20Banner%20PAG%20WEB%20INST-%20cita%20calendario_960_278.png');"></div>
+                <div class="fill" style="background-image:url('https://scontent.fsjo1-1.fna.fbcdn.net/v/t1.0-9/17426381_629255027260351_4913124547390830579_n.jpg?oh=8e56ac61a3d45bb773f3687e30746851&oe=5A37FA3A');"></div>
             </div>
 
 
               <div class="item">
                 <!-- Set the third background image using inline CSS below. -->
-                <div class="fill" style="background-image:url('http://www.una.ac.cr/images/resized/images/sampledata/slideshow/020%20Banner%20PAG%20WEB%20INST-%20cita%20calendario_960_278.png ');"></div>
+                <div class="fill" style="background-image:url('https://scontent.fsjo1-1.fna.fbcdn.net/v/t1.0-9/20294147_688304064688780_97640080953229707_n.jpg?oh=0df99f397c1ff7e0449477ace7cb0462&oe=59F8134C');"></div>
             </div>
 
         </div>
@@ -86,20 +126,21 @@ PARTE DE PABLOOOO
         <a class="right carousel-control" href="#myCarousel" data-slide="next">
             <span class="icon-next"></span>
         </a>
+	</div>
 
-        	
-    </header><!-- /header -->>
-
+    </div><!-- /header -->
+    <br><!--Espacio entre el carrusel y las noticias cuando esta en responsive-->
+    <div class="col-sm-3 " >      <!-- Segundo bloque de 4 columnas -->
+  	
+  	<div class="row">
     
-        <div class="sm-3">
-
 			<ul class="collapsible" data-collapsible="accordion">
 			  <li>
 			    <div class="collapsible-header card-panel"> <span class="blue-text text-darken-2">Entrega de Notas</span>
 			    </div>
 	
 
-			    <div class="collapsible-body"><p>Es donde estamos........</p></div>
+			    <div class="collapsible-body"><p>Estimados profesores, el dia de manana estaremos entregando las notas a los padres de familia, porfavor enviar un mensaje a cada uno de los padres, de parte del colegio humanistico.</p></div>
 			  </li>
 		
 			<li>
@@ -120,20 +161,12 @@ PARTE DE PABLOOOO
 			    <div class="collapsible-body"><p>Es donde estamos........</p></div>
 			  </li>
 
-			  <li>
-			    <div class="collapsible-header card-panel"> <span class="blue-text text-darken-2">Hola mundo</span>
-			   </div>
-			    <div class="collapsible-body"><p>Es donde estamos........</p></div>
-			  </li>
-		
 
 			</ul>
-      	
-            </div>
-
-
-
-
+		</div>
+		</div>
+</div>
+</div>
 
         <!-- Footer -->
         <footer align= "center">
