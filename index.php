7cdd6ad76695fd4d2c53062de9ced75828a95258
Adan Carranza Alfaro <adanca16@gmail.com>
2017-08-01 18:00:04 -0600

    CSS

--- index.php
+++ index.php
@@ -3,22 +3,117 @@
 <head>
 
 <meta charset="UTF-8">
+<!-- Latest compiled and minified CSS -->
+       <link rel="icon" type="image/png" href="https://upload.wikimedia.org/wikipedia/en/1/1f/Universidad_Nacional_Costa_Rica.png" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+         <link rel="stylesheet" href="estilos.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 
 	<title>Reclutamiento de Profesores, Campus Sarapiqui</title>
+
+	<LINK REL="stylesheet" TYPE="text/css" HREF="css/diseno.css">
+
 </head>
 <body>
-	<h1 >Ada </h1>
-	
+<!--  Cabecera de la pagina primcipal-->
 
-<?php 
-	require_once('Conexion.php');
-	$conexion = new Conexion();
-	$resultdo=$conexion->conexion()->query("SELECT * FROM tbproductos");
-	while($sql=$resultdo->fetch_assoc()){//Todos los valore de la tabla.
-		echo $sql['idproducto'].' '.$sql['nombreproducto'];
-	}
 
- ?> 
+<nav class="navbar navbar-default navbar-fixed-top" id="cajaCabecera">
+  <div class="container" >
+
+   	<p>Reclutamiento de profesores UNA</p>
+  </div>
+</nav>
+
+
+
+
+
+
+<nav class="navbar navbar-default navbar-fixed-bottom">
+  <div class="container">
+   	<p>Reclutamiento de profesores UNA</p>
+  </div>
+</nav>
+
+
+    <header id="myCarousel" class="carousel slide">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner" style="margin-top: ;">
+            <div class="item active">
+                <div class="fill" style="background-image:url('http://www.una.ac.cr/images/resized/images/sampledata/slideshow/022%20Banner%20PAG%20WEB%20INST-%20cita%20calendario_960_278.png ');"></div>
+            </div>
+            <div class="item">
+                <div class="fill" style="background-image:url('http://www.una.ac.cr/images/resized/images/sampledata/slideshow/023%20Banner%20PAG%20WEB%20INST-%20cita%20calendario_960_278.png ');"></div>
+             
+            </div>
+            <div class="item">
+                <div class="fill" style="background-image:url('http://www.una.ac.cr/images/resized/images/sampledata/slideshow/021%20Banner%20PAG%20WEB%20INST-%20cita%20calendario_960_278.png');"></div>
+            </div>
+        </div>
+        <!-- Controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="icon-prev"></span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="icon-next"></span>
+        </a>
+    </header>
+
+    
+
+  <hr class="star-primary">
+
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+            
+            <div class="col-lg-2 col-xs-1" align = "center">
+            </div>
+            <div class="col-lg-8 col-xs-10" align = "center" id="capaInformacion">
+            </div>
+
+            <div class="col-lg-2 col-xs-1" align = "center">
+            </div>
+        </div>
+
+        <hr>
+
+        <!-- Footer -->
+        <footer align= "center">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Creado bajo la normativa de Universidad Nacional de Costa Rica </p>
+                    <p>Compania: Carranza-Alfaro</p>
+                </div>
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
 
 
 </body>
